03e53ddfa4f237e60fc9a56d899e6f08a5aea418
jason <jasonjeontaekang@gmail.com>
2026-09-11 12:50:30 -0500

    Added goblinTwo
    Added goblinTwo to the game class.

--- game.py
+++ game.py
@@ -13,6 +13,11 @@ def main():
     goblin = Goblin("Scribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    goblinTwo = Goblin("Gribble")
+
+    print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+
     print("But no hero has answered the call... yet.")
 
 
